e5e23ba1d1f991c60462a69153f0fdeb7472192b
Angular CLI <angular-cli@angular.io>
2017-11-08 21:21:49 +1100

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'busrep-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'busrep';
+}
