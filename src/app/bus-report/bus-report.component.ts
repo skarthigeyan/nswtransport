fdf4970f0443ea6decc77307ca1715cb406a6bd2
Karthic Sankarajeyaraman <skarthic1212@gmail.com>
2017-11-10 02:07:55 +1100

    injecting the service

--- src/app/bus-report/bus-report.component.ts
+++ src/app/bus-report/bus-report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import {IBusorganisation} from './../models/busdata-model';
+import {BusServicesService} from './../bus-services.service';
 
 @Component({
   selector: 'busrep-busreport',
@@ -10,11 +11,13 @@ import {IBusorganisation} from './../models/busdata-model';
 })
 export class BusReportComponent implements OnInit {
 
-  showTable: boolean = false;
-  busServices : IBusorganisation[] =[{"organisation":"Sydney Buses","date":"25/09/2015","busData":[{"busId":"42612","routeVariant":"891 2 1","deviationFromTimetable":77},{"busId":"29016","routeVariant":"400 1 1","deviationFromTimetable":340},{"busId":"90467","routeVariant":"393 1 1","deviationFromTimetable":220},{"busId":"88836","routeVariant":"M20 1 0","deviationFromTimetable":-287},{"busId":"79367","routeVariant":"L21 2 1","deviationFromTimetable":347}]},{"organisation":"Westbus","date":"25/09/2015","busData":[{"busId":"94811","routeVariant":"664 2 1","deviationFromTimetable":164},{"busId":"62788","routeVariant":"UNKNOWN","deviationFromTimetable":null},{"busId":"14221","routeVariant":"834 1 1","deviationFromTimetable":423}]}];
-  constructor() { }
+  
+  busServices : IBusorganisation[];
+  constructor(private _busService: BusServicesService) {
+  }
 
   ngOnInit() {
+    this.busServices = this._busService.getBusServicesData();
   }
 
   saveNotes(notes){
